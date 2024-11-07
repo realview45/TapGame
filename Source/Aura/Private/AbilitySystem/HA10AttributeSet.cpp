0db85db0f2637d036781693dec0d357c29f27300
realview45 <kjink0125@gmail.com>
2024-11-07 23:33:32 +0900

    25-1

--- Source/Aura/Private/AbilitySystem/HA10AttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/HA10AttributeSet.cpp
@@ -2,4 +2,23 @@
 
 
 #include "AbilitySystem/HA10AttributeSet.h"
+//25
+#include "AbilitySystemComponent.h"
+//25-2
+#include "Net/UnrealNetwork.h"
 
+UHA10AttributeSet::UHA10AttributeSet()
+{
+
+}
+//25-2
+void UHA10AttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME_CONDITION_NOTIFY(UHA10AttributeSet, Health, COND_None, REPNOTIFY_Always);
+}
+
+void UHA10AttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UHA10AttributeSet, Health, OldHealth);
+}
--- Source/Aura/Public/AbilitySystem/HA10AttributeSet.h
+++ Source/Aura/Public/AbilitySystem/HA10AttributeSet.h
@@ -13,5 +13,16 @@ UCLASS()
 class AURA_API UHA10AttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
-	
+
+//25
+public:
+	UHA10AttributeSet();
+	//25-2
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Vital Attributes")
+	FGameplayAttributeData Health;
+
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
 };
