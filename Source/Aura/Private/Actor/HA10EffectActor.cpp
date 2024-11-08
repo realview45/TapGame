40cf826f8a3abde0ad7c2c7653219f5742df4713
realview45 <kjink0125@gmail.com>
2024-11-09 07:56:32 +0900

    38

--- Source/Aura/Private/Actor/HA10EffectActor.cpp
+++ Source/Aura/Private/Actor/HA10EffectActor.cpp
@@ -3,52 +3,79 @@
 
 #include "Actor/HA10EffectActor.h"
 
-#include "Components/SphereComponent.h"
+//38 remove unused include
+//#include "Components/SphereComponent.h"
 //27-3
-#include "AbilitySystemInterface.h"
-#include "AbilitySystem/HA10AttributeSet.h"
+//#include "AbilitySystemInterface.h"
+//#include "AbilitySystem/HA10AttributeSet.h"
+
+//38
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
+
 // Sets default values
 AHA10EffectActor::AHA10EffectActor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
+	
+	//38
+	//Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	//SetRootComponent(Mesh);
 
-	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
-	SetRootComponent(Mesh);
-
-	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
-	Sphere->SetupAttachment(GetRootComponent());
+	//Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
+	//Sphere->SetupAttachment(GetRootComponent());
+	SetRootComponent(CreateDefaultSubobject<USceneComponent>("SceneRoot"));
 }
-
-void AHA10EffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
-{
-	//Shouldn't do this!! const_cast
-	//27-3
-	if (IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(OtherActor))
-	{
-		//pure virtual function
-		const UHA10AttributeSet* HA10AttributeSet = Cast<UHA10AttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(UHA10AttributeSet::StaticClass()));
-		UHA10AttributeSet* MutableHA10AttributeSet = const_cast<UHA10AttributeSet*>(HA10AttributeSet);
-		MutableHA10AttributeSet->SetHealth(HA10AttributeSet->GetHealth() + 25.f);
-		//36
-		MutableHA10AttributeSet->SetMana(HA10AttributeSet->GetMana() + 25.f);
-		Destroy();
-	}
-}
-
-void AHA10EffectActor::OutOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
-{
-
-}
+//38
+//void AHA10EffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+//{
+//	//Shouldn't do this!! const_cast
+//	//27-3
+//	if (IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(OtherActor))
+//	{
+//		//pure virtual function
+//		const UHA10AttributeSet* HA10AttributeSet = Cast<UHA10AttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(UHA10AttributeSet::StaticClass()));
+//		UHA10AttributeSet* MutableHA10AttributeSet = const_cast<UHA10AttributeSet*>(HA10AttributeSet);
+//		MutableHA10AttributeSet->SetHealth(HA10AttributeSet->GetHealth() + 25.f);
+//		//36
+//		MutableHA10AttributeSet->SetMana(HA10AttributeSet->GetMana() + 25.f); 
+//		Destroy();
+//	}
+//}
+//
+//void AHA10EffectActor::OutOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+//{
+//
+//}
 
 // Called when the game starts or when spawned
 void AHA10EffectActor::BeginPlay()
 {
 	Super::BeginPlay();
-	
-	Sphere->OnComponentBeginOverlap.AddDynamic(this, &AHA10EffectActor::OnOverlap);
-	Sphere->OnComponentEndOverlap.AddDynamic(this, &AHA10EffectActor::OutOverlap);
+	//38	
+	//Sphere->OnComponentBeginOverlap.AddDynamic(this, &AHA10EffectActor::OnOverlap);
+	//Sphere->OnComponentEndOverlap.AddDynamic(this, &AHA10EffectActor::OutOverlap);
 
+}
+//38
+void AHA10EffectActor::ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplayEffect> GameplayEffectClass)
+{
+	//38
+	//IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(Target);
+	//if(ASCInterface)
+	//{
+	//	ASCInterface->GetAbilitySystemComponent();
+	//	UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Target);
+	//}
+	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Target);
+	if(TargetASC == nullptr)return;
+	check(GameplayEffectClass);
+	FGameplayEffectContextHandle EffectContextHandle = TargetASC->MakeEffectContext();
+	EffectContextHandle.AddSourceObject(this);
+	FGameplayEffectSpecHandle EffectSpecHandle = TargetASC->MakeOutgoingSpec(GameplayEffectClass, 1.f, EffectContextHandle);
+	//dereference pointer
+	TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
 }
 
 
--- Source/Aura/Public/Actor/HA10EffectActor.h
+++ Source/Aura/Public/Actor/HA10EffectActor.h
@@ -6,7 +6,8 @@
 #include "GameFramework/Actor.h"
 #include "HA10EffectActor.generated.h"
 
-class USphereComponent;
+//38
+//class USphereComponent;
 
 UCLASS()
 class AURA_API AHA10EffectActor : public AActor
@@ -17,22 +18,30 @@ public:
 	// Sets default values for this actor's properties
 	AHA10EffectActor();
 
-	UFUNCTION()
-	virtual void OnOverlap
-	(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
-	
-	UFUNCTION()
-	virtual void OutOverlap
-	(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	//38
+	//UFUNCTION()
+	//virtual void OnOverlap
+	//(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	//
+	//UFUNCTION()
+	//virtual void OutOverlap
+	//(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	//38
+	UFUNCTION(BlueprintCallable)
+	void ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplayEffect> GameplayEffectClass);
+	UPROPERTY(EditAnywhere, Category = "Applied Effects")
+	TSubclassOf<UGameplayEffect> InstantGameplayEffectclass;
+
 private:
 	//overlapvolume sphere
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<USphereComponent> Sphere;
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<UStaticMeshComponent> Mesh;
+	// //38
+	//UPROPERTY(VisibleAnywhere)
+	//TObjectPtr<USphereComponent> Sphere;
+	//UPROPERTY(VisibleAnywhere)
+	//TObjectPtr<UStaticMeshComponent> Mesh;
 };
