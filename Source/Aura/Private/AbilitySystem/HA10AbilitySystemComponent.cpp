50b89f40b8470a0c654b23bddcf08b7999328f84
realview45 <kjink0125@gmail.com>
2024-11-10 22:25:06 +0900

    55

--- Source/Aura/Private/AbilitySystem/HA10AbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/HA10AbilitySystemComponent.cpp
@@ -10,4 +10,14 @@ void UHA10AbilitySystemComponent::AbilityActorInfoSet()
 void UHA10AbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent, const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveEffectHandle)
 {
 	GEngine->AddOnScreenDebugMessage(1, 8.f, FColor::Blue, FString("Effect Applied!"));
+	//55
+	FGameplayTagContainer TagContainer;
+	EffectSpec.GetAllAssetTags(TagContainer);
+	for (const FGameplayTag& Tag : TagContainer)
+	{
+		//TODO:Broadcast the tah to the the WidgetController
+		//Tag.GetTageName() returns FName, Tag.ToString() returns FString put * to convert a wide character array(ArrayofTchar)
+		const FString Msg = FString::Printf(TEXT("GE Tag: %s"), *Tag.ToString());
+		GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor::Blue, Msg);
+	}
 }
