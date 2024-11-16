70f294bba821402e4eeb8d38aa9d974019ef604b
rv45 <kjink0125@gmail.com>
2024-11-17 08:22:09 +0900

    90

---
+++ Source/Aura/Public/UI/WidgetController/AttributeMenuWidgetController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/WidgetController/ObjectHA10WidgetController.h"
+#include "AttributeMenuWidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAttributeMenuWidgetController : public UObjectHA10WidgetController
+{
+	GENERATED_BODY()
+	
+};
