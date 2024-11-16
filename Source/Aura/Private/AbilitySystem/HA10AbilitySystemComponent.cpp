8783e41c5f51a2d94e1364482e606a09fbf960b0
rv45 <kjink0125@gmail.com>
2024-11-17 05:32:24 +0900

    87-5

--- Source/Aura/Private/AbilitySystem/HA10AbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/HA10AbilitySystemComponent.cpp
@@ -2,10 +2,21 @@
 
 
 #include "AbilitySystem/HA10AbilitySystemComponent.h"
+//87-5
+#include "HA10GameplayTags.h"
 //54
 void UHA10AbilitySystemComponent::AbilityActorInfoSet()
 {
 	OnGameplayEffectAppliedDelegateToSelf.AddUObject(this, &UHA10AbilitySystemComponent::EffectApplied);
+	//87-5
+	const FHA10GameplayTags& GameplayTags = FHA10GameplayTags::Get();
+	GEngine->AddOnScreenDebugMessage(
+		-1,
+		10.f,
+		FColor::Orange,
+		FString::Printf(TEXT("Tag: %s"), *GameplayTags.Attributes_Secondary_Armor.ToString())
+	);
+
 }
 void UHA10AbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent, 
 	const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveEffectHandle)
