a692e4be1af847e79889549c7e495e15ab37e663
rv45 <kjink0125@gmail.com>
2024-11-17 14:28:36 +0900

    93

--- Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -2,6 +2,10 @@
 //90
 
 #include "UI/WidgetController/AttributeMenuWidgetController.h"
+//93
+#include "AbilitySystem/HA10AttributeSet.h"
+#include "AbilitySystem/Data/MyAttributeInfo.h"
+#include "HA10GameplayTags.h"
 
 void UAttributeMenuWidgetController::BindCallbacksToDependencies()
 {
@@ -10,4 +14,10 @@ void UAttributeMenuWidgetController::BindCallbacksToDependencies()
 
 void UAttributeMenuWidgetController::BroadcastInitialValues()
 {
+	//93
+	UHA10AttributeSet* AS = CastChecked<UHA10AttributeSet>(AttributeSet);
+	check(AttributeInfo);
+	FHA10AttributeInfo Info = AttributeInfo->FindAttributeInfoForTag(FHA10GameplayTags::Get().Attributes_Primary_Strength);
+	Info.AttributeValue = AS->GetStrength();
+	AttributeInfoDelegate.Broadcast(Info);
 }
--- Source/Aura/Public/UI/WidgetController/AttributeMenuWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/AttributeMenuWidgetController.h
@@ -4,7 +4,18 @@
 
 #include "CoreMinimal.h"
 #include "UI/WidgetController/ObjectHA10WidgetController.h"
+
+//93
+#include "AbilitySystem/HA10AttributeSet.h"
+#include "AbilitySystem/Data/MyAttributeInfo.h"
+#include "HA10GameplayTags.h"
+
 #include "AttributeMenuWidgetController.generated.h"
+
+//93 define FAttributeInfoSignature
+struct FHA10AttributeInfo;
+class UMyAttributeInfo;
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FAttributeInfoSignature, const FHA10AttributeInfo&, Info);
 
 /**
  * 
@@ -18,4 +29,13 @@ public:
 	//HA10widget controller's function override 
 	virtual void BindCallbacksToDependencies() override; //update variables
 	virtual void BroadcastInitialValues() override; //initialize variables
+
+	//93
+	UPROPERTY(BlueprintAssignable, Category="GAS|Attributes")
+	FAttributeInfoSignature AttributeInfoDelegate;
+
+protected:
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UMyAttributeInfo>AttributeInfo;
+
 };
